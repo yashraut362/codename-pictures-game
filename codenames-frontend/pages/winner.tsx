5f4cba4f32465bd8e5d257da9baa8897554f8b9b
Yash Raut <yash.raut@hnrtech.com>
2025-02-02 02:22:23 +0530

    Add WavyBackground component for WinnerPage

--- codenames-frontend/pages/winner.tsx
+++ codenames-frontend/pages/winner.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useSocket } from "../context/SocketContext";
 import { useRouter } from 'next/navigation'
 import Confetti from 'react-confetti'
+import { WavyBackground } from "@/components/ui/wavy-background";
 
 const WinnerPage = () => {
     const { socket } = useSocket();
@@ -18,12 +19,23 @@ const WinnerPage = () => {
     }
     return (
         <>
-            <h1>{name} Team won the game</h1>
-            <button onClick={resetGame}>resetGame</button>
-            <Confetti
+            {/* <Confetti
                 width={1400}
                 height={1000}
-            />
+            /> */}
+            <WavyBackground className="max-w-4xl mx-auto ">
+                <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+                    {name} team won the game
+                </p>
+                <div class="text-center">
+                    <button onClick={resetGame} className="p-[3px] relative mt-10">
+                        <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+                        <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+                            Start New Game
+                        </div>
+                    </button>
+                </div>
+            </WavyBackground>
         </>
     );
 };
